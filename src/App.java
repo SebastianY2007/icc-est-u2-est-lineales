b6b5af059ee60397ece675d7b16b81c8bf088d07
Derlis Sebastian Yupangui Buestan <dyupanguib@est.ups.edu.ec>
2025-12-08 12:48:04 -0500

    Termindad

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import models.Node;
+import models.Person;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Node<Person> node1 = new Node<Person>(new Person("Juan", 30));
+        
+        Node<Person> node2 = new Node<Person>(new Person("Maria", 35));
+        
+        Node<Person> node3 = new Node<Person>(new Person("Pepito", 30));
+        
+        Node<Person> node4 = new Node<Person>(new Person("Diego", 35));
+
+        node1.setNext(node2);  // J -> M
+        node2.setNext(node3);  // M -> P
+        node3.setNext(node4);  // P -> D
+       
+
+        System.out.println(node1);
     }
 }
--- src/models/Node.java
+++ src/models/Node.java
@@ -1,3 +1,43 @@
-public class Node {
+package models;
 
+public class Node<T> {
+    
+    private T value; // UPS_2025
+    private Node<T> next; // Puntero
+    private Node<T> prev; // Anterior
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNode Value: " + value + ", Next: -> " + next;
+    }
 }
