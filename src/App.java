d4bf8195637213e360d3bf3a9bb8d1c0b1832a6e
Derlis Sebastian Yupangui Buestan <dyupanguib@est.ups.edu.ec>
2025-12-09 08:47:40 -0500

    Practica Terminada (Stack)

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
+import controllers.Stack;
 import models.Node;
 import models.Person;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
 
+        /* 
         Node<Person> node1 = new Node<Person>(new Person("Juan", 30));
         
         Node<Person> node2 = new Node<Person>(new Person("Maria", 35));
@@ -18,5 +21,24 @@ public class App {
        
 
         System.out.println(node1);
+        */
+
+        runStackExample();
+    }
+    
+
+    public static void runStackExample() {
+        Stack<Person> persons = new Stack<Person>();
+
+        persons.push(new Person("Juan", 30));
+        persons.push(new Person("Maria", 35));
+        persons.push(new Person("Pepito", 32));
+        persons.push(new Person("Diego", 31));
+        
+        System.out.println("Size: " + persons.size());
+        persons.printAllNodes();
+        System.out.println(persons.pop());
+        System.out.println("Size: " + persons.size());
+        persons.printAllNodes();
     }
 }
---
+++ src/controllers/Stack.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import java.util.EmptyStackException;
+import models.Node;
+
+public class Stack<T> {
+
+    private Node<T> top;
+    private int size;
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+    
+    // Método para insertar
+    public void push(T data) {
+        Node<T> newNode = new Node<T>(data);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+    }
+
+    // Método sacar y/o eliminar
+    public T pop() {
+        if (top == null) {
+            // Return null
+            throw new EmptyStackException();
+        }
+
+        T data = top.getValue();
+        top = top.getNext();
+        size--;
+
+        return data;
+    }   
+
+    // Retorna el primero pero sin eliminar
+    public T peek() {
+        if (top == null) 
+            return null;
+
+        return top.getValue();
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printAllNodes() {
+        Node<T> current = top;
+        System.out.println("Cima -> ");
+        while (current != null) {
+            System.out.println(current.getValue() + " -> ");
+            current = current.getNext();
+        }
+        System.out.println("FIN");
+    }
+}
